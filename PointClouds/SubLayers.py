fa2886b41ff939517245b07e79ce4b600c1af859
Zhang Shiyue <yuer3677@163.com>
2018-11-19 13:09:32 -0500

    without residual

--- PointClouds/SubLayers.py
+++ PointClouds/SubLayers.py
@@ -58,7 +58,7 @@ class MultiHeadAttention(nn.Module):
         output = output.permute(1, 2, 0, 3).contiguous().view(sz_b, len_q, -1) # b x lq x (n*dv)
 
         output = self.dropout(self.fc(output))
-        output = self.layer_norm(output + residual)
+        output = self.layer_norm(output)
 
         return output, attn
 
